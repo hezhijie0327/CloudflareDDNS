453c39719e3b4e29311e6e47928d3051380ff264
Zhijie He <hezhijie0327@hotmail.com>
2023-06-22 09:34:32 +0800

    docker.sh | Version 1.0.202306220934

---
+++ docker.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Parameter
+OWNER="hezhijie0327"
+REPO="cloudflareddns"
+TAG="latest"
+
+## Function
+# Get Latest Image
+function GetLatestImage() {
+    docker pull ${OWNER}/${REPO}:${TAG} && IMAGES=$(docker images -f "dangling=true" -q)
+}
+# Cleanup Current Container
+function CleanupCurrentContainer() {
+    if [ $(docker ps -a --format "table {{.Names}}" | grep -E "^${REPO}$") ]; then
+        docker stop ${REPO} && docker rm ${REPO}
+    fi
+}
+# Create New Container
+function CreateNewContainer() {
+    docker run --name ${REPO} --net host --restart=always \
+        -e XAUTHEMAIL="demo@zhijie.online" \
+        -e XAUTHKEY="123defghijk4567pqrstuvw890" \
+        -e ZONENAME="zhijie.online" \
+        -e RECORDNAME="demo.zhijie.online" \
+        -e TYPE="A_AAAA" \
+        -e TTL="1" \
+        -e STATICIP="auto" \
+        -e PROXYSTATUS="false" \
+        -e RUNNINGMODE="update" \
+        -e UPDATEFREQUENCY="900" \
+        -d ${OWNER}/${REPO}:${TAG}
+}
+# Cleanup Expired Image
+function CleanupExpiredImage() {
+    if [ "${IMAGES}" != "" ]; then
+        docker rmi ${IMAGES}
+    fi
+}
+
+## Process
+# Call GetLatestImage
+GetLatestImage
+# Call CleanupCurrentContainer
+CleanupCurrentContainer
+# Call CreateNewContainer
+CreateNewContainer
+# Call CleanupExpiredImage
+CleanupExpiredImage
