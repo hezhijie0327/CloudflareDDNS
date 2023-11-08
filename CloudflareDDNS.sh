c1e95cc6b8b851f3b84f6fd59c23fa67c68326ce
Zhijie He <hezhijie0327@hotmail.com>
2023-11-08 17:42:49 +0800

    CloudflareDDNS.sh | Version 1.3.3

--- CloudflareDDNS.sh
+++ CloudflareDDNS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.3.2
+# Current Version: 1.3.3
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/CloudflareDDNS.git" && bash ./CloudflareDDNS/CloudflareDDNS.sh -e demo@zhijie.online -k 123defghijk4567pqrstuvw890 -z zhijie.online -r demo.zhijie.online -t A -l 3600 -i auto -p false -m update
@@ -25,48 +25,36 @@ done
 function CheckConfigurationValidity() {
     if [ "${XAuthEmail}" == "" ]; then
         echo "An error occurred during processing. Missing (XAuthEmail) value, please check it and try again."
-        exit 1
     fi
     if [ "${XAuthKey}" == "" ]; then
         echo "An error occurred during processing. Missing (XAuthKey) value, please check it and try again."
-        exit 1
     fi
     if [ "${ZoneName}" == "" ]; then
         echo "An error occurred during processing. Missing (ZoneName) value, please check it and try again."
-        exit 1
     fi
     if [ "${RecordName}" == "" ]; then
         echo "An error occurred during processing. Missing (RecordName) value, please check it and try again."
-        exit 1
     fi
     if [ "${RunningMode}" == "" ]; then
         echo "An error occurred during processing. Missing (RunningMode) value, please check it and try again."
-        exit 1
     elif [ "${RunningMode}" != "create" ] && [ "${RunningMode}" != "update" ] && [ "${RunningMode}" != "delete" ]; then
         echo "An error occurred during processing. Invalid (RunningMode) value, please check it and try again."
-        exit 1
     fi
     if [ "${RunningMode}" == "create" ] || [ "${RunningMode}" == "update" ]; then
         if [ "${Type}" == "" ]; then
             echo "An error occurred during processing. Missing (Type) value, please check it and try again."
-            exit 1
         elif [ "${Type}" != "A" ] && [ "${Type}" != "AAAA" ] && [ "${Type}" != "A_AAAA" ]; then
             echo "An error occurred during processing. Invalid (Type) value, please check it and try again."
-            exit 1
         fi
         if [ "${TTL}" == "" ]; then
             echo "An error occurred during processing. Missing (TTL) value, please check it and try again."
-            exit 1
         elif [ "${TTL}" != "1" ] && [ "${TTL}" != "120" ] && [ "${TTL}" != "300" ] && [ "${TTL}" != "600" ] && [ "${TTL}" != "900" ] && [ "${TTL}" != "1800" ] && [ "${TTL}" != "3600" ] && [ "${TTL}" != "7200" ] && [ "${TTL}" != "18000" ] && [ "${TTL}" != "43200" ] && [ "${TTL}" != "86400" ]; then
             echo "An error occurred during processing. Invalid (TTL) value, please check it and try again."
-            exit 1
         fi
         if [ "${ProxyStatus}" == "" ]; then
             echo "An error occurred during processing. Missing (ProxyStatus) value, please check it and try again."
-            exit 1
         elif [ "${ProxyStatus}" != "true" ] && [ "${ProxyStatus}" != "false" ]; then
             echo "An error occurred during processing. Invalid (ProxyStatus) value, please check it and try again."
-            exit 1
         fi
     fi
 }
@@ -75,17 +63,14 @@ function CheckEnvironmentValidity() {
     which "curl" > "/dev/null" 2>&1
     if [ "$?" -eq "1" ]; then
         echo "curl has not been installed!"
-        exit 1
     fi
     which "dig" > "/dev/null" 2>&1
     if [ "$?" -eq "1" ]; then
         echo "dig has not been installed!"
-        exit 1
     fi
     which "jq" > "/dev/null" 2>&1
     if [ "$?" -eq "1" ]; then
         echo "jq has not been installed!"
-        exit 1
     fi
 }
 # Get Account Name
@@ -249,40 +234,32 @@ if [ "${RunningMode}" == "create" ]; then
     AccountName=$(GetAccountName)
     if [ "${AccountName}" == "invalid" ]; then
         echo "An error occurred during processing. Invalid (AccountName) value, please check your network connectivity, and try again."
-        exit 1
     elif [ "${AccountName}" == "false" ]; then
         echo "An error occurred during processing. Invalid (AccountName) value, please check (XAuthEmail) and (XAuthKey) value, and try again."
-        exit 1
     else
         echo "Current Account Name: ${AccountName}"
         # Call GetZoneID
         ZoneID=$(GetZoneID)
         if [ "${ZoneID}" == "invalid" ]; then
             echo "An error occurred during processing. Invalid (ZoneID) value, please check your network connectivity, and try again."
-            exit 1
         elif [ "${ZoneID}" == "false" ]; then
             echo "An error occurred during processing. Invalid (ZoneID) value, please check (ZoneName) value, and try again."
-            exit 1
         else
             function CreateQueue() {
                 # Call GetRecordID
                 RecordID=$(GetRecordID)
                 if [ "${RecordID}" == "invalid" ]; then
                     echo "An error occurred during processing. Invalid (RecordID) value, please check your network connectivity, and try again."
-                    exit 1
                 elif [ "${RecordID}" != "invalid" ] && [ "${RecordID}" != "false" ]; then
                     echo "An error occurred during processing. ${RecordName} has been existed."
-                    exit 1
                 else
                     # Call GetWANIP
                     WANIP=$(GetWANIP)
                     if [ "${WANIP}" == "invalid" ]; then
                         if [ "${Type}" == "A" ]; then
                             echo "An error occurred during processing. Invalid (WANIP) value, please check your IPv4 connectivity."
-                            exit 1
                         else
                             echo "An error occurred during processing. Invalid (WANIP) value, please check your IPv6 connectivity."
-                            exit 1
                         fi
                     else
                         echo "Current WAN IP: ${WANIP}"
@@ -292,7 +269,6 @@ if [ "${RunningMode}" == "create" ]; then
                             echo "No error occurred during processing. ${RecordName} has been created."
                         else
                             echo "An error occurred during processing. Invalid (POSTResponse) value, please check your network connectivity, and try again."
-                            exit 1
                         fi
                     fi
                 fi
@@ -311,30 +287,24 @@ elif [ "${RunningMode}" == "update" ]; then
     AccountName=$(GetAccountName)
     if [ "${AccountName}" == "invalid" ]; then
         echo "An error occurred during processing. Invalid (AccountName) value, please check your network connectivity, and try again."
-        exit 1
     elif [ "${AccountName}" == "false" ]; then
         echo "An error occurred during processing. Invalid (AccountName) value, please check (XAuthEmail) and (XAuthKey) value, and try again."
-        exit 1
     else
         echo "Current Account Name: ${AccountName}"
         # Call GetZoneID
         ZoneID=$(GetZoneID)
         if [ "${ZoneID}" == "invalid" ]; then
             echo "An error occurred during processing. Invalid (ZoneID) value, please check your network connectivity, and try again."
-            exit 1
         elif [ "${ZoneID}" == "false" ]; then
             echo "An error occurred during processing. Invalid (ZoneID) value, please check (ZoneName) value, and try again."
-            exit 1
         else
             function UpdateQueue() {
                 # Call GetRecordID
                 RecordID=$(GetRecordID)
                 if [ "${RecordID}" == "invalid" ]; then
                     echo "An error occurred during processing. Invalid (RecordID) value, please check your network connectivity, and try again."
-                    exit 1
                 elif [ "${RecordID}" == "false" ]; then
                     echo "An error occurred during processing. ${RecordName} has not been existed."
-                    exit 1
                 else
                     echo "Current Record ID: ${RecordID}"
                     # Call GetWANIP
@@ -342,10 +312,8 @@ elif [ "${RunningMode}" == "update" ]; then
                     if [ "${WANIP}" == "invalid" ]; then
                         if [ "${Type}" == "A" ]; then
                             echo "An error occurred during processing. Invalid (WANIP) value, please check your IPv4 connectivity."
-                            exit 1
                         else
                             echo "An error occurred during processing. Invalid (WANIP) value, please check your IPv6 connectivity."
-                            exit 1
                         fi
                     else
                         echo "Current WAN IP: ${WANIP}"
@@ -353,10 +321,8 @@ elif [ "${RunningMode}" == "update" ]; then
                         DNSRecord=$(GetDNSRecord)
                         if [ "${DNSRecord}" == "invalid" ]; then
                             echo "An error occurred during processing. Invalid (DNSRecord) value, please check your network connectivity, and try again."
-                            exit 1
                         elif [ "${DNSRecord}" == "false" ]; then
                             echo "An error occurred during processing. Invalid (DNSRecord) value, please check (ZoneName) and (RecordName) value, and try again."
-                            exit 1
                         else
                             if [ "${DNSRecord}" == "${WANIP}" ]; then
                                 echo "No error occurred during processing. WAN IP has not been changed."
@@ -368,7 +334,6 @@ elif [ "${RunningMode}" == "update" ]; then
                                     echo "No error occurred during processing. ${RecordName} has been updated."
                                 else
                                     echo "An error occurred during processing. Invalid (PUTResponse) value, please check your network connectivity, and try again."
-                                    exit 1
                                 fi
                             fi
                         fi
@@ -389,30 +354,24 @@ else
     AccountName=$(GetAccountName)
     if [ "${AccountName}" == "invalid" ]; then
         echo "An error occurred during processing. Invalid (AccountName) value, please check your network connectivity, and try again."
-        exit 1
     elif [ "${AccountName}" == "false" ]; then
         echo "An error occurred during processing. Invalid (AccountName) value, please check (XAuthEmail) and (XAuthKey) value, and try again."
-        exit 1
     else
         echo "Current Account Name: ${AccountName}"
         # Call GetZoneID
         ZoneID=$(GetZoneID)
         if [ "${ZoneID}" == "invalid" ]; then
             echo "An error occurred during processing. Invalid (ZoneID) value, please check your network connectivity, and try again."
-            exit 1
         elif [ "${ZoneID}" == "false" ]; then
             echo "An error occurred during processing. Invalid (ZoneID) value, please check (ZoneName) value, and try again."
-            exit 1
         else
             function DeleteQueue() {
                 # Call GetRecordID
                 RecordID=$(GetRecordID)
                 if [ "${RecordID}" == "invalid" ]; then
                     echo "An error occurred during processing. Invalid (RecordID) value, please check your network connectivity, and try again."
-                    exit 1
                 elif [ "${RecordID}" == "false" ]; then
                     echo "An error occurred during processing. ${RecordName} has not been existed."
-                    exit 1
                 else
                     echo "Current Record ID: ${RecordID}"
                     # Call GetDELETEResponse
@@ -421,7 +380,6 @@ else
                         echo "No error occurred during processing. ${RecordName} has been deleted."
                     else
                         echo "An error occurred during processing. Invalid (DELETEResponse) value, please check your network connectivity, and try again."
-                        exit 1
                     fi
                 fi
             }
